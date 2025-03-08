e1b4c69b6eea13dd4b7f8eb652fa0ee93a880378
csabeszko <kiss.csaba8@gmail.com>
2025-03-08 22:43:52 +0100

    Creating skeleton for tests

---
+++ Taxually.TechnicalTest/Taxually.Hosting.TechnicalTest_iTests/VatRegistrationTests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Taxually.Hosting.TechnicalTest_iTests
+{
+    [TestFixture]
+    class VatRegistrationTests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
